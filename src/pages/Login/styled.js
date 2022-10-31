b742ff67612a9e9378d6d72c72cf6108e90d83c6
rodrigo costa <xrodcostax@outlook.com>
2022-10-31 17:09:53 -0300

    Push Geral

--- src/pages/Login/styled.js
+++ src/pages/Login/styled.js
@@ -4,10 +4,12 @@ export const Title = styled.h1`
   background: green;
 
   small {
-    font-size: 10px;
+    font-size: 12pt;
+    margin-left: 15px;
     color: #999;
   }
 `;
+
 export const Paragrafo = styled.p`
-  font-size: 60px;
+  font-size: 80px;
 `;
